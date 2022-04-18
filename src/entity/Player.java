b6d2b3cca37aa6841e4187049eb341f4700f87bf
Serious07 <Serious-07@inbox.ru>
2022-04-18 12:59:40 +0700

    Add Walking Player Sprites and basic Sprite Drawing for player

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -2,6 +2,10 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.KeyHandler;
@@ -15,31 +19,66 @@ public class Player extends Entity {
 		this.keyH = keyH;
 		
 		setDefaultValues();
+		getPlayerImage();
+	}
+	
+	public void getPlayerImage() {
+		try {
+			up1 = ImageIO.read(getClass().getResource("/player/boy_up_1.png"));
+			up2 = ImageIO.read(getClass().getResource("/player/boy_up_2.png"));
+			down1 = ImageIO.read(getClass().getResource("/player/boy_down_1.png"));
+			down2 = ImageIO.read(getClass().getResource("/player/boy_down_2.png"));
+			left1 = ImageIO.read(getClass().getResource("/player/boy_left_1.png"));
+			left2 = ImageIO.read(getClass().getResource("/player/boy_left_2.png"));
+			right1 = ImageIO.read(getClass().getResource("/player/boy_right_1.png"));
+			right2 = ImageIO.read(getClass().getResource("/player/boy_right_2.png"));
+		} catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 	
 	public void setDefaultValues(){
 		x = 100;
 		y = 100;
 		speed = 4;
+		direction = "down";
 	}
 	
 	public void Update() {
 		if(keyH.upPressed == true){
+			direction = "up";
 			y -= speed;
 		}
 		if(keyH.downPressed == true){
+			direction = "down";
 			y += speed;
 		}
 		if(keyH.leftPressed == true){
+			direction = "left";
 			x -= speed;
 		}
 		if(keyH.rightPressed == true){
+			direction = "right";
 			x += speed;
 		}
 	}
 	
 	public void Draw(Graphics2D g2) {
-		g2.setColor(Color.white);
-		g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+		BufferedImage image = null;
+		switch (direction) {
+		case "up":
+			image = up1;
+			break;
+		case "down":
+			image = down1;
+			break;
+		case "left":
+			image = left1;
+			break;
+		case "right":
+			image = right1;
+			break;
+		}
+		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
 	}
 }
