b9c6e9b2fe245109ba07aefec615df627d8c4e58
Serious07 <Serious-07@inbox.ru>
2022-04-19 12:23:58 +0700

    Add Camera, and world map

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -18,15 +18,21 @@ public class GamePanel extends JPanel implements Runnable{
 	public final int tileSize = originalTileSizes * scale;
 	public final int maxScreenCol = 16;
 	public final int maxScreenRow = 12;
-	final int screenWidth = tileSize * maxScreenCol;
-	final int screenHeight = tileSize * maxScreenRow;
+	public final int screenWidth = tileSize * maxScreenCol;
+	public final int screenHeight = tileSize * maxScreenRow;
+	
+	// WORLD SETTINGS
+	public final int maxWorldCol = 50;
+	public final int maxWorldRow = 50;
+	public final int worldWidth = tileSize * maxWorldCol;
+	public final int worldHeight = tileSize * maxWorldRow;
 	
 	int fps = 60;
 	
 	TileManager tileManager = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
-	Player player = new Player(this, keyH);
+	public Player player = new Player(this, keyH);
 	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
